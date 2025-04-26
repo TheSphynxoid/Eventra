fea7c7e6eb02e15eb89e6177331bb86e190ae30e
The Sphynx <jmail.hamza@gmail.com>
2025-04-26 12:07:47 +0100

    Set up project structure and update dependencies; rename artifact and adjust FXML views

--- src/main/java/com/thesphynx/services/IService.java
+++ src/main/java/com/thesphynx/services/IService.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IService <T>{
-    public void ajouter(T t) throws SQLException;
-    public void modifier(T t) throws SQLException;
-    public void supprimer(int id) throws SQLException;
-    public List<T> afficher() throws SQLException;
+    public void Ajouter(T t) throws SQLException;
+    public void Modifier(T t) throws SQLException;
+    public void Supprimer(int id) throws SQLException;
+    public List<T> Afficher() throws SQLException;
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.esprit3b5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.thesphynx to javafx.fxml;
-    exports com.thesphynx;
-}
